17b9fab8f74fddbfbcbfde193d5530a67710bd96
Chris Snowden <chrissnowden35@gmail.com>
2023-02-09 23:05:02 +1100

    Remove JaxRS annotations from IBookRepository

--- src/main/java/org/cps/quarkus/starting/respository/IBookRepository.java
+++ src/main/java/org/cps/quarkus/starting/respository/IBookRepository.java
@@ -2,14 +2,12 @@ package org.cps.quarkus.starting.respository;
 
 import org.cps.quarkus.starting.model.Book;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.PathParam;
 import java.util.List;
 import java.util.Optional;
 
 public interface IBookRepository {
-    @GET
+
     List<Book> getAllBooks();
 
-    Optional<Book> getBook(@PathParam("id") int id);
+    Optional<Book> getBook(int id);
 }
